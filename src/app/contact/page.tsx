dd0e1488b2377fa11ec2d5cd742bd2f35b56c714
unknown <your.email@example.com>
2025-03-18 18:54:27 +0530

    updated meta tags

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -3,8 +3,8 @@ import { Metadata } from 'next';
 
 
 export const metadata: Metadata = {
-    title: "Contact Us",
-    description: "Pack My Product is a platform that helps you pack your products for your next move.",
+    title: "Contact Pac My Product: Get in Touch for Packaging Products & Gifting Solutions",
+    description: "Collaborate with Pac My Product for innovative gifting kits and packaging products with personalized printing. Let’s make your brand’s image more appealing together!",
 };
 
 export default function App() {
